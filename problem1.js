5bcb8f19fe6fa8f9d9d8b31862317de603173a43
spicysamson <paulinoedselgrageda.samson@bicol-u.edu.ph>
2025-10-20 01:23:17 +0800

    mali pa rin pero ge langs

--- problem1.js
+++ problem1.js
@@ -1,3 +1,22 @@
+// Read CSV and separate into left_arr and right_arr
+const fs = require("fs");
+const path = require("path");
+const csvFile = path.join(__dirname, "problem1inputssssss.csv");
+const csvText = fs.readFileSync(csvFile, "utf-8");
+
+// let left_arr = [];
+// let right_arr = [];
+// csvText
+//   .trim()
+//   .split("\n")
+//   .forEach((line) => {
+//     const [left, right] = line.split(",");
+//     left_arr.push(Number(left));
+//     right_arr.push(Number(right));
+//   });
+
+// console.log("left_arr:", left_arr.slice(0, 10));
+// console.log("right_arr:", right_arr.slice(0, 10));
 //2 arrays
 //find smallest in left and smallest in right, match them up
 //get their total and append in a new array
@@ -7,25 +26,34 @@
 //1 Even number length nung sorted, no problem, lahat may kapair
 // Odd number length, may isang walang kapair so default na x - 0 na yun.
 
-left_arr = [14, 67, 62, 67, 73, 3, 50, 54, 67, 70];
-right_arr = [56, 38, 19, 33, 57, 24, 85, 14, 27, 60];
+let left_arr = [3, 4, 2, 1, 3, 3];
+let right_arr = [4, 3, 5, 3, 9, 3];
 
-combined_arr = left_arr.concat(...right_arr);
-sorted_arr = combined_arr.sort((a, b) => b - a); //descending sort
-console.log(sorted_arr);
-sorted_arr_length = sorted_arr.length;
-let distance_arr = [];
-if (sorted_arr_length % 2 === 0) {
-  for (let i = 0; i < sorted_arr.length; i += 2) {
-    let distance = sorted_arr[i] - sorted_arr[i + 1];
-    distance_arr.push(distance);
+function getTotalDistance(left_arr, right_arr) {
+  sorted_left_arr = left_arr.sort((a, b) => a - b);
+  sorted_right_arr = right_arr.sort((a, b) => a - b);
+
+  let combined_arr = [...sorted_left_arr, ...sorted_right_arr];
+
+  let distance_arr = [];
+  let combined_arr_length = combined_arr.length;
+
+  for (i = 0, i < sorted_left_arr.length; i++; ) {
+    for (y = combined_arr_length / 2, y < combined_arr_length; y++; ) {
+      let distance = combined_arr[i] - combined_arr[y];
+      console.log(distance);
+      let absoluteDistance = Math.abs(distance);
+      distance_arr.push(absoluteDistance);
+    }
   }
-  console.log(distance_arr);
+
+  total_distance = 0;
+  for (let x = 0; x < distance_arr.length; x++) {
+    total_distance += distance_arr[x];
+  }
+
+  console.log(total_distance);
+  return total_distance;
 }
 
-total_distance = 0;
-for(let x = 0; x < distance_arr.length; x++ ){
-    total_distance += distance_arr[x]
-}
-
-console.log(total_distance)
+getTotalDistance(left_arr, right_arr);
