b02ad0908eb245d988cb9b4da7b2f04c1924131d
spicysamson <paulinoedselgrageda.samson@bicol-u.edu.ph>
2025-10-20 12:40:38 +0800

    Refactor level processing logic to enhance safety checks and add comments for clarity

--- problem2.js
+++ problem2.js
@@ -6,11 +6,11 @@
 
 // Store the grid in a 2D array
 const levels = [
-  [7, 6, 4, 2, 1],
+  [7, 6, 4, 2, 1], //desc
   [1, 2, 7, 8, 9],
-  [9, 7, 6, 2, 1],
+  [9, 7, 6, 2, 1], //desc
   [1, 3, 2, 4, 5],
-  [8, 6, 4, 4, 1],
+  [8, 6, 4, 4, 1], //desc
   [1, 3, 6, 7, 9],
 ];
 
@@ -20,9 +20,38 @@ let unsafelevels_count = 0;
 //Displaying of values first in 2d array
 for (let x = 0; x < levels.length; x++) {
   let row_arr = [];
-  for (let i = 0; i < levels[x].length; i++) {
+  let isAscending = false;
+  let isDescending = false;
+  let isReportSafe = true;
+  let direction = null; // 'desc' or 'asc'
+  for (let i = 0; i < levels[x].length - 1; i++) {
     // console.log(`Row ${x}: ${levels[x][i]}`);
-    row_arr.push(levels[x][i]);
+    let diff = levels[x][i] - levels[x][i + 1];
+    //Check if descending
+    if (levels[x][i] > levels[x][i + 1]) {
+      if (direction === null) direction = "desc";
+      isDescending = true;
+      if (direction !== "desc" || !(diff >= 1 && diff <= 3)) {
+        isReportSafe = false;
+        break;
+      }
+    } else if (levels[x][i] < levels[x][i + 1]) {
+      isAscending = true;
+      if (direction === null) direction = "asc";
+      if (direction !== "asc" || !(diff <= -1 && diff >= -3)) {
+        isReportSafe = false;
+        break;
+      }
+      //   let isSafe = diff >= -3 && diff < 0;
+    } else {
+      isReportSafe = false;
+      break;
+    }
   }
-  console.log(`Row ${x}: ${row_arr}`)
+  //   console.log(`Row ${x}: ${row_arr}`)
+  if (!isAscending) {
+    console.log(
+      `Row ${x}: is descendng? ${!isAscending} & is REPORT SAFE? ${isReportSafe}`
+    );
+  }
 }
